b60d3550707abd22fb320d150e6622b02fd41e57
xsweetpea <ejw322@nyu.edu>
2014-12-04 19:02:20 -0500

    edited index.js and login.hbs

--- finalproject/routes/index.js
+++ finalproject/routes/index.js
@@ -11,15 +11,50 @@ var DNA = mongoose.model('DNA');
 router.get('/', function(req, res) {
 //	res.redirect('/login');
   res.render('index', { title: 'Imagine your DNA~' });
-  setTimeout(function(){
+  setTimeout(function(){ //wait abit before moving to login page
   	res.redirect('/login');
   }, 1000);
 });
+
+//LOGIN
 router.get('/login', function(req, res){
-	res.redirect('/login');
+	res.render('login', {title: "Login!"});
 });
+router.post('/login', function(req, res){
+	User.findOne({userName: req.body.userName}, function(err, dbUser, count){
+		console.log("Current user is : " + curUserName);
+		console.log("The data base user is: " + dbUser);
+		if (dbUser == null){
+			//user doesnt exist
+			//maybe bring up popup that will ask to register or relogin?
+			res.redirect("/login");
+		}
+		else if (req.body.passWord == dbUser.passWord){
+			//user exists and password is correct
+			res.redirect(("/account/" + dbUser.userName));
+		}
+		else{ 
+			//maybe bring up popup that will say incorrect password, register, relogin?
+			res.redirect("/login");
+		}
+	});
+	
+});
+
+//REGISTER
 router.get('/login/register', function(req, res){
 	res.redirect('/register');
 });
+
+//USER
+
+//USER - SLUG
+
+//USER - SLUG - ADD DNA
+
+//USER - SLUG - FIND SEQUENCE
+
+//
+
 
 module.exports = router;
